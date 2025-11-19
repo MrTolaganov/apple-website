6fd245ef4efc301ce21a9b74a21890169b781e84
MrTolaganov <otabektolaganov69@gmail.com>
2025-11-19 10:47:21 +0500

    implement hero section

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,12 @@
 import Navbar from "./components/shared/navbar.jsx";
+import HeroSection from "./components/sections/hero.section.jsx";
 
 export default function App() {
   return (
     <main>
       <Navbar />
+
+      <HeroSection />
     </main>
   );
 }
